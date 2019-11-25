04c959cf71f6fd01a838bd7c8da205d09bd0c784
Ricardo Silva <ricardoatsilva1@gmail.com>
2019-11-24 21:26:03 -0300

    Creating third screen

---
+++ lib/tutorialScreens/tutorial3.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:tutorial/tutorial.dart';
+import 'package:tutorial/strings.dart';
+
+class Tutorial3State extends StatelessWidget {
+  final Widget dots;
+  Tutorial3State(this.dots);
+  @override
+  Widget build(BuildContext context) {
+
+    return Column(children: [
+          Header(),
+          Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: ImageView('assets/images/tutorial3.png')
+          ),
+          dots,
+          Padding(
+            padding: EdgeInsets.only(top: 30),
+            child: Tutorial3Content()
+          )
+      ]);
+  }
+
+}
+
+class Tutorial3Content extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: 85, right: 85, bottom: 16),
+          child: MessageText(Strings.tutorial3Title)
+        ),
+        Padding(
+          padding: EdgeInsets.only(left: 60, right: 60, bottom: 60),
+          child: DescriptionText(Strings.tutorial3Message)
+        ),
+        ButtonView(Strings.tutorial3Button)
+      ]
+    );
+  }
+
+}
